047592813b4864a38b4dbae91a993ea45173acf2
rvlwldev <rvlwlasd77@gmail.com>
2025-09-19 02:05:26 +0900

    fix: 유저 생성 기능 버그 수정

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '@/modules/users/user.entity';
@@ -9,16 +9,24 @@ export class UserService {
   constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
 
   async createUser(req: UserCreateRequest): Promise<User> {
-    const user = await this.repo.findOne({ where: [{ id: req.id }, { nickname: req.nickname }] });
+    const user = await this.repo.findOne({
+      where: [{ id: req.id }, { nickname: req.nickname }],
+    });
 
-    if (user != null && user.id == req.id) {
-      throw new ConflictException('이미 사용 중인 ID 입니다.');
-    }
+    if (user) {
+      if (user.id == req.id) {
+        throw new ConflictException('이미 사용 중인 ID 입니다.');
+      }
 
-    if (user != null && user.nickname == req.nickname) {
-      throw new ConflictException('이미 사용 중인 닉네임입니다.');
+      if (user.nickname == req.nickname) {
+        throw new ConflictException('이미 사용 중인 닉네임입니다.');
+      }
     }
 
     return this.repo.save(this.repo.create(req));
+  }
+
+  async findUserById(id: string) {
+    return await this.repo.findOne({ where: { id } });
   }
 }
